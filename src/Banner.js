56a3fc6b996d7f6be4fd1cea2dc72734ef206b29
yasin <buntyvirani82@gmail.com>
2021-01-20 05:35:34 -0800

    banner updated

--- src/Banner.js
+++ src/Banner.js
@@ -18,7 +18,10 @@ function Banner() {
     }
     fetchData();
   }, []);
-  console.log(movies);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -36,7 +39,10 @@ function Banner() {
           <button className="banner_button">Play</button>
           <button className="banner_button">My List </button>
         </div>
-        <h1 className="banner_description">{movies?.overview}</h1>
+
+        <h1 className="banner_description">
+          {truncate(movies?.overview, 200)}
+        </h1>
       </div>
       <div className="banner_fadeBottom" />
     </header>
