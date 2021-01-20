1c76f525f1c229d21b06c609a395e76a484b9591
yasin <buntyvirani82@gmail.com>
2021-01-20 03:53:55 -0800

    applied styles and some title showing for movies

--- src/Banner.js
+++ src/Banner.js
@@ -17,7 +17,22 @@ function Banner() {
     fetchData();
   }, []);
   console.log(movies);
-  return <header></header>;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movies?.backdrop_path}")`,
+        backdropPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movies?.title || movies?.name || movies?.original_name}
+        </h1>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
